fc1e2cfbc1941cdbf87bc4de6b6f8b4c7b2f9401
ram <ramkrishan@kontikilabs.com>
2019-03-15 20:27:52 +0530

    Updates

---
+++ precache-manifest.bf2d8355c68faa73ce7d2c60bf6f7b5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "44f2a4e03c62d612bc50",
+    "url": "/React-Emoji-Search/static/js/runtime~main.44f2a4e0.js"
+  },
+  {
+    "revision": "b6e306d9399fc518f79a",
+    "url": "/React-Emoji-Search/static/js/main.b6e306d9.chunk.js"
+  },
+  {
+    "revision": "2be004faa655df450af2",
+    "url": "/React-Emoji-Search/static/js/2.2be004fa.chunk.js"
+  },
+  {
+    "revision": "b6e306d9399fc518f79a",
+    "url": "/React-Emoji-Search/static/css/main.28b37e7a.chunk.css"
+  },
+  {
+    "revision": "2be004faa655df450af2",
+    "url": "/React-Emoji-Search/static/css/2.78bb1a5e.chunk.css"
+  },
+  {
+    "revision": "9cf3f0d0854cb0a77caaaa9fbbfd642a",
+    "url": "/React-Emoji-Search/index.html"
+  }
+];
